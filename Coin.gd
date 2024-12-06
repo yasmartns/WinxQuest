d0ff401aad4af7ddbd224cf62f81ea8a5b7f8694
yasmartns <yasmingomes2977@gmail.com>
2024-12-05 23:02:23 -0300

    Score

--- Coin.gd
+++ Coin.gd
@@ -5,5 +5,6 @@ func _on_animation_player_animation_finished(_anim_name : String):
 
 func _on_area_entered(area):
 	if area.is_in_group("Player"):
+		ScoreManager.add_score(1)
 		$AnimationPlayer.play("CoinCollected")
 		$CollisionShape2D.call_deferred("set_disabled", true)
