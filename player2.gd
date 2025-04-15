8375650d34692193715aabc9124a17cd2a6c484d
Pedro Fonseca <160919616+yasmartns@users.noreply.github.com>
2025-04-15 16:02:43 -0300

    mudancas

--- player2.gd
+++ player2.gd
@@ -1,58 +1,51 @@
 extends Area2D
 
-var direction : Vector2
+var direction : Vector2 = Vector2.ZERO
 var min_positions : Vector2 = Vector2(20, 176)
 var max_positions : Vector2 = Vector2(520, 756)
 var speed : int = 350
 var score : int
 var coins_collected_in_level : int
-var can_move : bool
+var can_move : bool = true
 
 func _process(delta):
+	if get_parent().game_over:
+		$AnimatedSprite2D.animation = "Death"
+		return
+
 	if can_move:
-		position += direction * speed * delta
+		handle_input()
+		position += direction.normalized() * speed * delta
 		position.x = clamp(position.x, min_positions.x, max_positions.x)
 		position.y = clamp(position.y, min_positions.y, max_positions.y)
 	else:
-		if get_parent().game_over:
-			$AnimatedSprite2D.animation = "Death"
-			return
-		
 		$AnimatedSprite2D.animation = "Idle"
 
-func _on_up_button_button_up():
-	can_move = false
 
-func _on_down_button_button_up():
-	can_move = false
+func handle_input():
+	direction = Vector2.ZERO
 
-func _on_right_button_button_up():
-	can_move = false
+	if Input.is_action_pressed("ui_up"):
+		direction.y -= 1
+		$AnimatedSprite2D.animation = "Back"
 
-func _on_left_button_button_up():
-	can_move = false
-	$AnimatedSprite2D.flip_h = false
+	if Input.is_action_pressed("ui_down"):
+		direction.y += 1
+		$AnimatedSprite2D.animation = "Front"
 
-func _on_up_button_button_down():
-	direction = Vector2.UP
-	can_move = true
-	$AnimatedSprite2D.animation = "Back"
+	if Input.is_action_pressed("ui_left"):
+		direction.x -= 1
+		$AnimatedSprite2D.animation = "Walk"
+		$AnimatedSprite2D.flip_h = true
 
-func _on_down_button_button_down():
-	direction = Vector2.DOWN
-	can_move = true
-	$AnimatedSprite2D.animation = "Front"
+	if Input.is_action_pressed("ui_right"):
+		direction.x += 1
+		$AnimatedSprite2D.animation = "Walk"
+		$AnimatedSprite2D.flip_h = false
 
-func _on_right_button_button_down():
-	direction = Vector2.RIGHT
-	can_move = true
-	$AnimatedSprite2D.animation = "Walk"
+	if direction == Vector2.ZERO:
+		$AnimatedSprite2D.animation = "Idle"
 
-func _on_left_button_button_down():
-	direction = Vector2.LEFT
-	can_move = true
-	$AnimatedSprite2D.animation = "Walk"
-	$AnimatedSprite2D.flip_h = true
 
 func _on_area_entered(area):
 	if area.is_in_group("Coin"):
@@ -60,7 +53,7 @@ func _on_area_entered(area):
 		score += 1
 		get_parent().get_node("UI/CoinsLabel").text = "Flores: " + str(score)
 		get_parent().get_node("UI/CoinsLabelAnimationPlayer").play("ScoreIncreased")
-		
+
 		if coins_collected_in_level == get_parent().level:
 			coins_collected_in_level = 0
 			get_parent().level_passed()
